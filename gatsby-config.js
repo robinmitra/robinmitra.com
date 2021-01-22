2c8ee912bb31380b45641ad6081e4a54b9300f25
Robin Mitra <robinmitra1@gmail.com>
2021-01-22 19:39:50 +0000

    Personalise and remove unused things.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Robin Mitra`,
+    description: `Robin's personal website`,
+    author: `@robinmitra`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
