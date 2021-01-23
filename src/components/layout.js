41644d09ae9bf25e5e5efa7486eae9d91ca7b778
Robin Mitra <robinmitra1@gmail.com>
2021-01-23 17:01:10 +0000

    Use local fonts.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "@fontsource/pacifico";
+import "@fontsource/source-code-pro";
 
 import styles from "./layout.module.css";
 import Footer from "./footer";
