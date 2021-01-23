08718b28cf74c29f35cb4a9b5f4bb7a7e4a3b797
Robin Mitra <robinmitra1@gmail.com>
2021-01-23 21:25:45 +0000

    Improve layout of social icons for mobile.

--- src/components/socialLinks.js
+++ src/components/socialLinks.js
@@ -9,22 +9,26 @@ const SocialLinks = () => {
     <ul className={styles.container}>
       <li>
         <a href="https://github.com/robinmitra/">
-          <FontAwesomeIcon icon={faGithub} /> GitHub
+          <FontAwesomeIcon icon={faGithub} />
+          <span className={styles.linkText}>GitHub</span>
         </a>
       </li>
       <li>
         <a href="https://twitter.com/robinmitra/">
-          <FontAwesomeIcon icon={faTwitter} /> Twitter
+          <FontAwesomeIcon icon={faTwitter} />
+          <span className={styles.linkText}>Twitter</span>
         </a>
       </li>
       <li>
         <a href="https://steamcommunity.com/id/robinmitra/">
-          <FontAwesomeIcon icon={faSteam} /> Steam
+          <FontAwesomeIcon icon={faSteam} />
+          <span className={styles.linkText}>Steam</span>
         </a>
       </li>
       <li>
         <a href="https://www.linkedin.com/in/robinmitra/">
-          <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+          <FontAwesomeIcon icon={faLinkedin} />
+          <span className={styles.linkText}>LinkedIn</span>
         </a>
       </li>
     </ul>
