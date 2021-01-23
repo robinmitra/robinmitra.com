a38648794716f35ca4eb31d5f8e7af5630a7418c
Robin Mitra <robinmitra1@gmail.com>
2021-01-23 21:01:27 +0000

    Use social icons, and improve layout and spacing.

--- src/components/socialLinks.js
+++ src/components/socialLinks.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 import styles from "./socialLinks.module.css";
 
 const SocialLinks = () => {
   return (
     <ul className={styles.container}>
-      <li><a href="https://github.com/robinmitra/">GitHub</a></li>
-      <li><a href="https://twitter.com/robinmitra/">Twitter</a></li>
-      <li><a href="https://www.linkedin.com/in/robinmitra/">LinkedIn</a></li>
+      <li>
+        <a href="https://github.com/robinmitra/">
+          <FontAwesomeIcon icon={faGithub} /> GitHub
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/robinmitra/">
+          <FontAwesomeIcon icon={faTwitter} /> Twitter
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/robinmitra/">
+          <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+        </a>
+      </li>
     </ul>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Body from "../components/body";
-import SocialLinks from "../components/socialLinks";
 import Projects from "../components/projects";
 
 const IndexPage = () => (
@@ -11,7 +10,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Body>
       <p>I'm Robin, a software engineer with a passion for building things for the cloud.</p>
-      <SocialLinks />
     </Body>
     <Projects />
   </Layout>
